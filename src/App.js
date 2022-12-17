c2f2122e918e3444be457c4c997ae56b00a616fe
Iuliia <tretiakova.iuliia@gmail.com>
2022-12-17 11:13:10 +0300

    правки

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     return (
         <>
             <Routes>
-                <Route path='/' element={<Layout/>}>
+                <Route element={<Layout/>}>
                     <Route path='/' index element={<Services/>}/>
                     <Route path='/:serviceId/details' element={<ServicesDetail/>}/>
                     <Route path='/search' index element={<Task1/>}/>
